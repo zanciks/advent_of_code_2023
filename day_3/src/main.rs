d2580797e1a5835dfd9ac4343e918d6e340b617a
Zanciks <Zackarysemancik@gmail.com>
2023-12-18 20:24:05 +0000

    Day 4 part 1 with a struct to help for part 2?

--- day_3/src/main.rs
+++
@@ -1,9 +0,0 @@
-use std::fs;
-
-fn main() {
-    let mut contents = fs::read_to_string("src/prompt.txt").expect("Error reading prompt.txt");
-    for line in contents.lines() {
-        println!("{}", line);
-    }
-    println!("Hello, world!");
-}
--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -1,52 +1,59 @@
 #![allow(dead_code, unused_mut, unused_variables)]
 use std::fs;
 
-fn main() {
-    let mut contents = fs::read_to_string("src/prompt.txt").expect("Error reading prompt.txt");
-    part_one(contents);
+#[derive(Debug, Clone)]
+pub struct Card {
+    index: i32, // card number
+    winners: Vec<i32>,
+    candidates: Vec<i32>
 }
 
-fn part_one(contents: String) {
-    let mut sum: i32 = 0;
-    for line in contents.lines() {
-        let mut count: u32 = 0;
-        let line = clean_lines(line);
-        let mut parts = line.split("|");
-        let winners = winners(parts.next().unwrap());
-        let candidates = candidates(parts.next().unwrap());
-        for candidate in candidates {
-            if winners.contains(&candidate){
-                count += 1;
+impl Card {
+    fn new(line: &str) -> Self {
+        let mut parts = line.split(":");
+        let index = get_index(parts.next().unwrap());
+        let next_part = parts.next().unwrap();
+        let mut sub_parts = next_part.split("|");
+        let winners = get_numbers(sub_parts.next().unwrap());
+        let candidates = get_numbers(sub_parts.next().unwrap());
+        
+        Self {
+            index,
+            winners,
+            candidates
+        }
+    }
+    fn count_winning_numbers(&self) -> u32 {
+        let mut sum: u32 = 0;
+        for candidate in &self.candidates {
+            if self.winners.contains(candidate) {
+                sum += 1;
             }
         }
-        sum += count_to_sum(count);
+        return sum;
     }
-    println!("{}", sum);
+    fn points(&self) -> i32 {
+        let count = self.count_winning_numbers();
+        let points = count_to_sum(count);
+        return points;
+    }
 }
 
-fn clean_lines(line: &str) -> &str  {
-    let mut parts = line.split(":");
-    let first = parts.next();
-    return parts.next().unwrap();
+fn get_index(parts: &str) -> i32 {
+    for smaller_part in parts.split_ascii_whitespace() {
+        if smaller_part != "Card" {
+            return smaller_part.parse().unwrap();
+        }
+    }
+    return 0;
 }
 
-fn winners(line: &str) -> Vec<i32> {
-    let mut winners = vec![];
-    for  number in line.split_ascii_whitespace() {
-        winners.push(number.parse().unwrap());
+fn get_numbers(line_part: &str) -> Vec<i32> {
+    let mut output = vec![];
+    for number in line_part.split_ascii_whitespace() {
+        output.push(number.parse().unwrap())
     }
-
-    return winners;
-}
-
-fn candidates(line: &str) -> Vec<i32> {
-    let mut candidates = vec![];
-
-    for number in line.split_ascii_whitespace() {
-        candidates.push(number.parse().unwrap());
-    }
-
-    return candidates;
+    return output;
 }
 
 fn count_to_sum(count: u32) -> i32 {
@@ -56,4 +63,18 @@ fn count_to_sum(count: u32) -> i32 {
         return 2_i32.pow(count - 1)
     }
     return 0;
+}
+
+fn main() {
+    let mut contents = fs::read_to_string("src/prompt.txt").expect("Error reading prompt.txt");
+    part_one(contents.clone());
+}
+
+fn part_one(contents: String) {
+    let mut sum: i32 = 0;
+    for line in contents.lines() {
+        let card = Card::new(line);
+        sum += card.points()
+    }
+    println!("{}", sum);
 }
